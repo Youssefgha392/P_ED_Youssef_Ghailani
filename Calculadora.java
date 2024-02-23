cc1b35667b907a54066663979f611ab9a88a67b6
youssefGH <youssefghailani03@gmail.com>
2024-02-23 15:21:26 +0100

    adding the Calculadora clase al repositorio

---
+++ Calculadora.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.calculadora;
+
+/**
+ *
+ * @author Alumno Tarde
+ */
+public class Calculadora {
+private int ans;
+        
+  
+
+    public Calculadora() {
+       ans=0; 
+    }
+    
+    
+     public int Dividir(int a, int b) {
+         if (b==0) {
+             return 0 ;
+         }
+         ans=a/b;
+         return ans;
+    }
+}
